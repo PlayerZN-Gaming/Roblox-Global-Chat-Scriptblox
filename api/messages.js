26a6b55d08cc56d6270e04a69025b96e1416ea01
PlayerZN-Gaming <maralitcristeta@gmail.com>
2025-11-24 15:36:15 +0800

    Update messages.js

--- api/messages.js
+++ api/messages.js
@@ -1,27 +1,9 @@
-export default async function handler(req, res) {
-  // 🔥 Insert your keys here
-  const BIN_ID = "6923e199ae596e708f6ceed2";
-  const MASTER_KEY = "$2a$10$gsW4zzNGmXVvFclb.hFPheWIZhmqIWRobGSMh55RJaN3JI6.o60rK";
+import messages from './_data.js';
 
-  const r = await fetch(`https://api.jsonbin.io/v3/b/${BIN_ID}/latest`, {
-    headers: { "X-Master-Key": MASTER_KEY }
-  });
-
-  const data = await r.json();
-  const messages = data.record.messages || [];
-
-  const html = `
-  <!DOCTYPE html>
-  <html>
-  <head><title>Messages</title></head>
-  <body style="font-family: Arial; padding: 20px;">
-    <h2>Global Messages</h2>
-    ${messages
-      .map(m => `<p><b>${m.user}:</b> ${m.message}</p>`)
-      .join("")}
-  </body>
-  </html>`;
-
-  res.setHeader("Content-Type", "text/html");
-  res.status(200).send(html);
+export default function handler(req, res) {
+  if (req.method === 'GET') {
+    res.status(200).json(messages);
+  } else {
+    res.status(405).end();
+  }
 }
